c97923feb306b9a3c46b9b29ca1a288eedb09775
yaniv <yanivprusman@gmail.com>
2026-04-04 01:25:02 +0300

    #10: add feedback-lib to PT app

--- frontend/public-transportation/app/layout.tsx
+++ frontend/public-transportation/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import { FeedbackChat } from '@automate/feedback-lib/FeedbackChat';
 
 export const metadata: Metadata = {
   title: 'Public Transportation',
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body>
         {children}
+        <FeedbackChat />
       </body>
     </html>
   );
