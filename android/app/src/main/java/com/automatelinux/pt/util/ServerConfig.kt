6d55c2f7d8880f3e455a7a7edce29f89e6d6760f
yaniv <yanivprusman@gmail.com>
2026-04-29 22:34:03 +0300

    Update WG IPs from 10.0.0.x to 10.7.0.x
    WireGuard subnet renumbered from 10.0.0.0/24 to 10.7.0.0/24.
    Updated network_security_config and ServerConfig peer URLs/labels.

--- android/app/src/main/java/com/automatelinux/pt/util/ServerConfig.kt
+++ android/app/src/main/java/com/automatelinux/pt/util/ServerConfig.kt
@@ -10,18 +10,18 @@ object ServerConfig {
     private val port = BuildConfig.SERVER_PORT
 
     val servers = listOf(
-        "http://10.0.0.2:$port",
-        "http://10.0.0.1:$port",
-        "http://10.0.0.4:$port",
-        "http://10.0.0.6:$port",
+        "http://10.7.0.2:$port",
+        "http://10.7.0.1:$port",
+        "http://10.7.0.4:$port",
+        "http://10.7.0.6:$port",
         if (BuildConfig.FEEDBACK_ENABLED) "https://pt.dev.ya-niv.com" else "https://pt.prod.ya-niv.com"
     )
 
     val serverLabels = mapOf(
-        "http://10.0.0.2:$port" to "Desktop",
-        "http://10.0.0.1:$port" to "NUC (Leader)",
-        "http://10.0.0.4:$port" to "Ubuntu Levtov",
-        "http://10.0.0.6:$port" to "RPi Ubuntu",
+        "http://10.7.0.2:$port" to "Desktop",
+        "http://10.7.0.1:$port" to "NUC (Leader)",
+        "http://10.7.0.4:$port" to "Ubuntu Levtov",
+        "http://10.7.0.6:$port" to "RPi Ubuntu",
         (if (BuildConfig.FEEDBACK_ENABLED) "https://pt.dev.ya-niv.com" else "https://pt.prod.ya-niv.com") to "Public"
     )
 
